09e007d0c5576d953a728716e90554295c237403
Michael Mathews <micmath@gmail.com>
2009-07-13 14:55:58 +0100

    Updated tests to better detect problems with slow async and fast sync loads of the same module.

--- test/mock/glow/1.0.0/widgets/widgets.js
+++ test/mock/glow/1.0.0/widgets/widgets.js
@@ -7,15 +7,21 @@ gloader.module({
 	}
 });
 
-gloader.module({
-	name: 'glow.widgets.Panel',
-	library: ['glow', '1.0.0'],
-	depends: [
-		['glow', '1.0.0', 'glow.widgets']
-	],
-	
-	builder: function(glow) {
-		glow.widgets.Panel = function() {
-		};
-	}
-});
+function createModule() {
+	gloader.module({
+		name: 'glow.widgets.Panel',
+		library: ['glow', '1.0.0'],
+		depends: [
+			['glow', '1.0.0', 'glow.widgets']
+		],
+		
+		builder: function(glow) {
+			glow.widgets.Panel = function() {
+			};
+		}
+	});
+}
+
+// simulate a slow network connection?
+if (window.withDelay) { setTimeout(createModule, 1000); }
+else createModule();
